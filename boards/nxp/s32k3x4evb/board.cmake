b01f4312c8933e3531cef47909c5a1767dc8d854
Sachin Kumar <sachinkumarsnp@gmail.com>
2026-01-06 23:10:41 +0530

    boards: nxp: s32k3x4evb: added support for s32k3x4-T172-Review Comments Closed
    Introduces support for the NXP S32K3X4EVB board, including board
    configuration files, device tree sources, documentation, and
    defconfig.
    
    Adds the nxp_pemicro runner for flashing and debugging via the
    PeMicro gdb server, and registers it in the west runners.
    
    This enables Zephyr builds and development for the S32K344 MCU
    using OpenSDA and PeMicro tools.
    
    Testes with blinky and hello_world sample application
    
    Signed-off-by: Sachin Kumar <sachinkumarsnp@gmail.com>

--- boards/nxp/s32k3x4evb/board.cmake
+++ boards/nxp/s32k3x4evb/board.cmake
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2026 Sachin Kumar
+# SPDX-FileCopyrightText: Copyright The Zephyr Project Contributors
 # SPDX-License-Identifier: Apache-2.0
 
 board_set_debugger_ifnset(nxp_pemicro)
@@ -9,6 +9,12 @@ board_runner_args(nxp_pemicro
   "--soc-family-name=S32K3xx"
   "--device=NXP_S32K3xx_S32K344"
 )
+
+if(${CONFIG_XIP})
+  board_runner_args(nxp_pemicro "--loadTo=flash")
+else()
+  board_runner_args(nxp_pemicro "loadTo=sram")
+endif()
 
 
 set(ZEPHYR_RUNNERS nxp_pemicro ${ZEPHYR_RUNNERS})
